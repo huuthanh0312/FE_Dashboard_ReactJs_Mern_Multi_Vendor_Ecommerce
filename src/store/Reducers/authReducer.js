36389da6c3f895b75cc4d21bc46ec7d85786b32b
huuthanh0312 <huuthanhnguyen0312@gmail.com>
2024-10-11 23:32:24 +0700

    Error and Notification

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -3,14 +3,16 @@ import api from "../../api/api";
 
 export const admin_login = createAsyncThunk(
   'auth/admin_login',
-  async (info) => {
-    console.log(info);
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    //console.log(info);
     try {
 
-      const { data } = await api.post('/admin-login', info, { withCredentials: true });
-      console.log(data);
+      const data = await api.post('/admin-login', info, { withCredentials: true })
+      //console.log(data)
+      return fulfillWithValue(data)
     } catch (error) {
-
+      //console.log(error.response.data);
+      return rejectWithValue(error.response.data)
     }
   }
 )
@@ -24,14 +26,32 @@ export const authReducer = createSlice({
     userInfo: ''
   },
   reducers: {
-
+    // message clear function reudx
+    messageClear: (state, _) => {
+      state.errorMessage = ""
+    }
   },
-  extraReducers: () => {
-
+  // loader check state
+  extraReducers: (builder) => {
+    builder
+      .addCase(admin_login.pending, (state, { payload }) => {    // get status and data BE pending 404
+        state.loader = true;
+      })
+      .addCase(admin_login.rejected, (state, { payload }) => {   // get status and data BE pending 404
+        state.loader = false;
+        state.errorMessage = payload.error
+        console.log(payload)
+      })
+      .addCase(admin_login.fulfilled, (state, { payload }) => {   // get status and data BE success 200
+        state.loader = false;
+        state.successMessage = payload.data.message
+        console.log(payload)
+      })
   }
 
 })
 
+export const { messageClear } = authReducer.actions
 export default authReducer.reducer
 
 
--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { admin_login } from '../../store/Reducers/authReducer';
-
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { admin_login, messageClear } from '../../store/Reducers/authReducer';
+import { ClipLoader, PropagateLoader } from 'react-spinners';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom'
 
 const AdminLogin = () => {
+  const navigate = useNavigate() // điều hướng trang
   const dispatch = useDispatch() //kết nối component với Redux store để có thể gửi action và thay đổi state toàn cục của ứng dụng.
-
+  const { loader, errorMessage, successMessage } = useSelector(state => state.auth) //state loader
+  const [showPassword, setShowPassword] = useState(false); // state show password
   const [state, setState] = useState({
     email: "",
     password: "",
   })
+
 
   const inputHandle = (e) => {
     setState({
@@ -18,12 +24,42 @@ const AdminLogin = () => {
     })
   }
 
+  //submit form login
   const submit = (e) => {
     e.preventDefault()
     dispatch(admin_login(state))
-
   }
 
+  // use Effect check toast message error
+  useEffect(() => {
+    if (errorMessage) {
+      toast.error(errorMessage)
+      dispatch(messageClear()) //message clear function reudx
+    }
+    if (successMessage) {
+      toast.success(successMessage)
+      dispatch(messageClear()) //message clear function reudx
+      navigate('/')
+    }
+  }, [errorMessage, successMessage])
+
+  //style css PropagateLoader spinners
+  const overrideStyle = {
+    display: 'flex',
+    margin: '0 auto',
+    height: '16px',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
+  // show hide password
+  const showPasswordClick = () => {
+    if (showPassword === true) {
+      setShowPassword(false);
+    } else {
+      setShowPassword(true);
+    }
+  };
   return (
     <div className='min-w-screen min-h-screen bg-[#f4f4f5] flex justify-center items-center'>
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
@@ -39,20 +75,37 @@ const AdminLogin = () => {
             </div>
             <h3 className="font-semibold text-lg text-center">👋</h3>
             <form onSubmit={ submit }>
-              <div className='flex flex-col w-full gap-1 mb-3'>
+              <div className='flex flex-col w-full gap-1 mb-3 relative'>
                 <label htmlFor='email'>Email</label>
-                <input onChange={ inputHandle } value={ state.email }
+                <input disabled={ loader ? true : false }
+                  onChange={ inputHandle } value={ state.email }
                   className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md'
                   type='email' name='email' placeholder='Email' id='email' required />
+                {
+                  loader ? <ClipLoader size={ 20 } color='#f77001' className='absolute top-10 right-3 end-0 items-center justify-center' /> : <></>
+                }
               </div>
-              <div className='flex flex-col w-full gap-1 mb-3'>
+              <div className='flex flex-col w-full gap-1 mb-3 relative'>
                 <label htmlFor='password'>Password</label>
-                <input onChange={ inputHandle } value={ state.password }
+                <input disabled={ loader ? true : false }
+                  onChange={ inputHandle } value={ state.password }
+                  type={ showPassword ? 'text' : 'password' }
                   className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md'
-                  type='password' name='password' placeholder='Password' id='password' required />
+                  name='password' placeholder='Password' id='password' required />
+                <button
+                  type="button"
+                  disabled={ loader ? true : false }
+                  className="absolute top-7 end-0 p-3.5 rounded-e-md"
+                  onClick={ () => showPasswordClick() }>
+                  { showPassword ? <FaEye /> : <FaEyeSlash /> }
+                </button>
+
               </div>
-              <button className='bg-slate-700 w-full hover:shadow-blue-300/hover:shadow-lg text-white rounded-md px-7 py-2 mt-3 mb-3 hover:bg-sky-700'>
-                Sign In</button>
+              <button disabled={ loader ? true : false } className='bg-slate-700 w-full hover:shadow-blue-300/hover:shadow-lg text-white rounded-md px-7 py-2 mt-3 mb-3 hover:bg-sky-700'>
+                {
+                  loader ? <PropagateLoader size={ 10 } color='#f77001' cssOverride={ overrideStyle } /> : 'Login'
+                }
+              </button>
 
             </form>
           </div>
